f7f2667c264b5d7cb02d2ff3d577a1389db62238
Armando Torres <131007737+Armandopru@users.noreply.github.com>
2024-09-26 20:57:22 -0500

    Add Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://Portafolio.dev.github.io',
+  base: 'my-repo',
 });
+
